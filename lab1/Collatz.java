369094b016939d1d46156ec0236a6b0b85ce6fca
BlackKnife <lding1003@gmail.com>
2022-06-02 07:00:27 -0700

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,12 +5,13 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        // when n is even
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        // when n is odd
+        else {
             return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
 
